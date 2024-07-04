ad16acbd2e32b03e02c1742d52e588a361cc14d1
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2024-07-04 05:12:09 -0600

    adding slider

---
+++ src/components/main/worldmap/CardValoration.jsx
@@ -0,0 +1,58 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 2
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+const CardValoration = () => {
+    const valoration=[
+        {
+            city:"Barcelona",
+            valoration:4,
+            message:"Este es un mensaje de valoración de la ciudad random que está elegida. Este es un mensaje de valoración de la ciudad random que está elegida."
+        },{
+            city:"Granada",
+            valoration:5,
+            message:"Este es un mensaje de valoración de la ciudad random que está elegida. Este es un mensaje de valoración de la ciudad random que está elegida."
+        },{
+            city:"Madrid",
+            valoration:5,
+            message:"Este es un mensaje de valoración de la ciudad random que está elegida. Este es un mensaje de valoración de la ciudad random que está elegida."
+        },{
+            city:"Zaragoza",
+            valoration:3,
+            message:"Este es un mensaje de valoración de la ciudad random que está elegida. Este es un mensaje de valoración de la ciudad random que está elegida."
+        },
+    ];
+  return (
+    <div className="cardValoration">
+        <Carousel responsive={responsive} arrows={true}
+  showDots={true}  ssr={true} infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={1000}>
+            {valoration.map((data) => (
+                <div>
+                    <h2>{data.city}</h2>
+                    <p>{data.message}</p>
+                </div>))}
+        </Carousel>
+    </div>
+  )
+}
+
+export default CardValoration
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/header/Header"
 import AirCare from "../components/main/airCare/AirCare"
 import WindToday from "../components/main/windToday/WindToday"
+import CardValoration from "../components/main/worldmap/CardValoration"
 import Worldmap from "../components/main/worldmap/Worldmap"
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <main>
         <Header/>
         <Worldmap />
+        <CardValoration />
         <WindToday />
         <AirCare />
     </main>
